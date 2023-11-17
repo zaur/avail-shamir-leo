2aa3c2a87ff1c739302ede03c5655d07a50babc0
Zaur <zaur@agamov.ru>
2023-11-18 02:43:10 +0300

    edited run.sh and README

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # shamir.aleo
 
 ## Build Guide
-
-To compile this Aleo program, run:
-```bash
-snarkvm build
-```
-
-To execute this Aleo program, run:
-```bash
-snarkvm run hello
-```
+See run.sh  for sample commands to create shares, and restoring secret from shares.
--- run.sh
+++ run.sh
@@ -7,17 +7,11 @@ then
 fi
 
 
-# leo run create_shares "77777field" "3u8" "8391471947992374field" "[aleo1gz527q3dt4axqjj2a5zfxattf8djh38dql79tvrn69pja93njc9qxhavjj, aleo1gz527q3dt4axqjj2a5zfxattf8djh38dql79tvrn69pja93njc9qxhavjj, aleo1q8lpp29mtp8hzwlvzqf2qzkweyn0gk92x98ujddztkug32e6cyxqhya349, aleo1q8lpp29mtp8hzwlvzqf2qzkweyn0gk92x98ujddztkug32e6cyxqhya349, aleo1mqdnfnjxnpfdkyzewqsup45sw37g3ssyndxg7nt7hgssm7zx3vxslknm3p]"
-
-# leo run restore_secret_from_shares \
-#  "{x: 1field, \
-#    y: 922309144816301324502543109173820703316407336830053895717015341727054907705field }" \
-#   "{x: 2field, \
-#    y: 6845343146619967649889744972728947308153161740629396538520498078005765457140field }"  \
-#    "{x: 3field, \
-#     y: 880178506554258127663955713102286751758464541089900272539981297001313248000field }"
+# create 5 shares for secret "77777", with a thresdhold of 3, seed of 8391471947992374 and 5 addresses.
+leo run create_shares "77777field" "3u8" "8391471947992374field" "[aleo1gz527q3dt4axqjj2a5zfxattf8djh38dql79tvrn69pja93njc9qxhavjj, aleo1gz527q3dt4axqjj2a5zfxattf8djh38dql79tvrn69pja93njc9qxhavjj, aleo1q8lpp29mtp8hzwlvzqf2qzkweyn0gk92x98ujddztkug32e6cyxqhya349, aleo1q8lpp29mtp8hzwlvzqf2qzkweyn0gk92x98ujddztkug32e6cyxqhya349, aleo1mqdnfnjxnpfdkyzewqsup45sw37g3ssyndxg7nt7hgssm7zx3vxslknm3p]"
 
 
+# restore the secret (77777) from 3 shares. The number of shares provided is passed as a first argument.
 leo run restore_secret_from_n_shares \
     "3u8" \
     "[ \
@@ -27,5 +21,16 @@ leo run restore_secret_from_n_shares \
     {x: 0field, y: 0field }, \
     {x: 0field, y: 0field } \
     ]"
+
+
+
+# leo run restore_secret_from_shares \
+#  "{x: 1field, \
+#    y: 922309144816301324502543109173820703316407336830053895717015341727054907705field }" \
+#   "{x: 2field, \
+#    y: 6845343146619967649889744972728947308153161740629396538520498078005765457140field }"  \
+#    "{x: 3field, \
+#     y: 880178506554258127663955713102286751758464541089900272539981297001313248000field }"
+
 
 
